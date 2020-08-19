1d2e2fe58b835673c559a3720ae5e9cc4750edef
dmitry <roddg86@gmail.com>
2020-08-19 10:22:49 +0900

    init commit

---
+++ src/main/java/ru/roddg/MoneyBox/MoneyBoxApplication.java
@@ -0,0 +1,13 @@
+package ru.roddg.MoneyBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/roddg/MoneyBox/MoneyBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.roddg.MoneyBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
